2a7353b0f8a11fcf9edc861943e594ca49f640a8
vamsikrishna7-github <vamsikrishna.nagidi@gmail.com>
2025-09-08 20:44:58 +0530

    db changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,12 +78,28 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "neondb",
+        "USER": "neondb_owner",
+        "PASSWORD": "npg_N2YdMupEB7Gh",
+        "HOST": "ep-damp-thunder-a1zxphgb-pooler.ap-southeast-1.aws.neon.tech",
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+            "channel_binding": "require",
+        },
     }
 }
+
 
 
 # Password validation
